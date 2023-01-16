81606f16f69d93014a7c5694862af6ba7abe263d
otaviobarbosa617 <barbosaoapb@gmail.com>
2023-01-16 11:47:17 -0500

    Form take 1

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1.Pages;
+
+public class IndexModel : PageModel
+{
+    //declarations
+    public string FirstNumber { get; set; }
+    public string SecondNumber { get; set; }
+    public string ThirdNumber { get; set; }
+    
+    private readonly ILogger<IndexModel> _logger;
+
+    //methods
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
